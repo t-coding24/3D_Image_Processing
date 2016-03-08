4a727770017070ef6fcb2a79a704a6838f725d7a
Tomoki Yamashita <t.coding.24@gmail.com>
2016-03-08 10:58:12 +0900

    Test commit

---
+++ 3D_Image_Processing/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//Return paths of refurred image
+vector<string> SearchImageMode(vector<string> all_str,string searchType)
+{
+	vector<string> refurredImagePaths;
+	int size = all_str.size();
+	for(int i = 0; i < size; ++i) {
+		size_t pos = all_str[i].find(searchType);
+		if (pos != string::npos) {
+			refurredImagePaths.push_back(all_str[i]);
+		}
+	}
+	return refurredImagePaths;
+}
+
+void main(int argc, char** argv)
+{
+	vector<string> inputPaths;
+	for (int i = 0; i < argc; ++i) {
+		inputPaths.push_back((string)argv[i]);
+	}
+
+	vector<string> bmodePaths = SearchImageMode(inputPaths, "bmode");
+	vector<string> bmodeOriginalPaths = SearchImageMode(inputPaths, "original");
+	vector<string> dopplerPaths = SearchImageMode(inputPaths, "doppler");
+}
